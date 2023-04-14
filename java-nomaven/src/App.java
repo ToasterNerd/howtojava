54d88cc4cdeea98e5b10e8aba1465bd25ec05d38
ToasterNerd <80899936+ToasterNerd@users.noreply.github.com>
2023-04-14 15:26:05 -0300

    Add files via upload

--- java-nomaven/src/App.java
+++ java-nomaven/src/App.java
@@ -1,5 +1,21 @@
+import lombok.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+
+        MyClass myClass = new MyClass();
+        myClass.setName("Lombok");
+        System.out.println(myClass.getName());
+    }
+
+
+    @Getter
+    @Setter
+    static class MyClass {
+        private String name;
     }
 }
+
+
